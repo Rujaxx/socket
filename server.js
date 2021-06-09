032beda17603c61b52d27fce6197423240d010b3
Rujaxx <shaikhrujan@gmail.com>
2021-06-09 19:38:59 +0530

    socket server

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const socketIO = require("socket.io"); 
+
+
+const app = express();
+const server = http.createServer(app);
+
+// setup the port our backend app will run on
+const PORT = 3030;
+const NEW_MESSAGE_EVENT = "new-message-event";
+
+const io = socketIO(server, {
+  cors: true,
+  origins:["localhost:3000"]
+});
+
+
+
+const room = "general"
+
+io.on("connection", (socket) => {
+    socket.join(room);
+    
+    socket.on(NEW_MESSAGE_EVENT, (data) => {
+      io.in(room).emit(NEW_MESSAGE_EVENT, data);
+    });
+  
+    socket.on("disconnect", () => {
+      socket.leave(room);
+    });
+  });
+
+  server.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`);
+  });
